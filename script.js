b2b0928a0602295c091265404fb229ddf532ac39
unknown <saurab.shrestha99@gmail.com>
2020-01-05 14:27:36 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// console.log("hellow");
+// restart game button
+var restart = document.querySelector("#b");
+
+// grabs all squares
+var squares = document.querySelectorAll('td');
+
+
+
+// clear all squares
+function clearBoard() {
+    for(var i = 0; i < squares.length; i++){
+        squares[i].textContent = '';
+    }
+}
+
+restart.addEventListener('click', clearBoard);
+
+
+// check the sq markers
+
+function changeMarker(){
+    if(this.textContent === '') {
+        this.textContent = 'X';
+    }else if (this.textContent === 'X') {
+        this.textContent = 'O';
+    }else {
+        this.textContent = '';
+    }
+}
+
+
+// for loop to add event listeners to all the squares
+for(var i = 0; i<squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
+}
